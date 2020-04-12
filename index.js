1e287e48d509b2f30f0429f34456cbc068d643ec
Víctor M. Reyes <vm.reyesal@gmail.com>
2020-04-12 15:28:56 -0600

    Mostrando todas las portadas

--- index.js
+++ index.js
@@ -18,23 +18,29 @@ app.get('/', (req, res) => {
 app.get('/api/v1/books/:name', (req, res, body) => {
     const book = req.params;
     request.get(OPEN_LIBRARY_SEARCH + book.name, { json: true }, (error, response, body) => {
-        if (body.docs.length > 0 ){
+        if (body.docs.length > 0) {
             let books = []
             body.docs.forEach((book) => {
+                let covers = [];
+                if (book.isbn) {
+                    book.isbn.forEach(c => {
+                        covers.push(`http://covers.openlibrary.org/b/isbn/${c}-L.jpg`)
+                    })
+                }
                 books.push({
                     "name": book.title_suggest,
-                    "cover" : book.isbn != undefined ? `http://covers.openlibrary.org/b/isbn/${book.isbn[0]}-L.jpg`: null,
-                    "author_picture": book.author_key != undefined ? `http://covers.openlibrary.org/a/olid/${book.author_key[0]}-S.jpg` : null,
-                    "author_name": book.author_name != undefined ? book.author_name : null,
-                    "language": book.language != undefined ? book.language : null,
-                    "subject": book.subject != undefined ? book.subject : null,
-                    "publish_year": book.publish_year != undefined ? book.publish_year : null,
-                    "publisher": book.publisher != undefined ? book.publisher : null,
-                    "isbn" : book.isbn != undefined ? book.isbn : null
+                    "cover": book.isbn ? covers : null,
+                    "author_picture": book.author_key ? `http://covers.openlibrary.org/a/olid/${book.author_key}-S.jpg` : null,
+                    "author_name": book.author_name ? book.author_name : null,
+                    "language": book.language ? book.language : null,
+                    "subject": book.subject ? book.subject : null,
+                    "publish_year": book.publish_year ? book.publish_year : null,
+                    "publisher": book.publisher ? book.publisher : null,
+                    "isbn": book.isbn ? book.isbn : null
                 })
             });
-            res.send({"book" : books})
-        } res.send({"message" : "Not found result"})
+            res.send({ "book": books })
+        } else { res.send({ "message": "Not found result" }) }
     })
 });
 
